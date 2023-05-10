f7410bbaa57a9475bf2b4dd8c22db1fba63131e8
prozorovadim <113033296+prozorovadim@users.noreply.github.com>
2023-05-10 11:38:45 +0300

    Update Выбор специализации. Итоговое задание.py

--- Выбор специализации. Итоговое задание.py
+++ Выбор специализации. Итоговое задание.py
@@ -1,10 +1,3 @@
 list = []
 length = int(input())
-for j in range (length):
-    a=input()
-    list.append(a)
-new = []
-for i in range (len (list)):
-    if len(list[i])<=3:
-        new.append(list[i])
-print(new)
+
